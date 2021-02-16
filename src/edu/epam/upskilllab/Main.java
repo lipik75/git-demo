1173d827bea8e507ffde3aa55f843829a1d3a68d
Sivukha Ihar <lipik75@gmail.com>
2021-02-16 14:21:18 +0300

    create a simple program Hello

---
+++ src/edu/epam/upskilllab/Main.java
@@ -0,0 +1,17 @@
+package edu.epam.upskilllab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please, enter your name:");
+        String name = reader.readLine();
+        reader.close();
+        System.out.println("Hello " + name);
+    }
+}
